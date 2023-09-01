804e304755e5d22d80c0b3b05dd7fa632b455708
Taylor Wagstaff <taylorwagstaff@gmail.com>
2023-09-01 23:00:12 +1200

    more styling

--- tongarirocinema/src/app/contact/page.js
+++ tongarirocinema/src/app/contact/page.js
@@ -1,3 +1,9 @@
+import './contact.css'
+
 export default function Page() {
-  return <h1>Hello, contact Page!</h1>
+  return (
+    <div className="contact-container">
+      <h1>Hello, contact Page!</h1>
+    </div>
+  )
 }
